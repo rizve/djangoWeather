d8b8ba5618782e9b1ac63a02f092bc63c68b5385
Rizve <rizve.cse@gmail.com>
2023-03-05 01:39:46 +0600

    WeatherApp Complete Like Tutorial

--- lookup/views.py
+++ lookup/views.py
@@ -1,7 +1,35 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request,'home.html',{})
+    
+    import json
+    import requests
+
+    if request.method == "POST":
+        # zipcode = request.POST['zipcode']
+        zipco = request.POST["abc"]
+        # return render(request,'home.html',{'zipco': zipco })
+    
+    
+        # api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=25&API_KEY=2193A50C-794E-49D8-AFD4-8353E4146A51")
+        api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode="+ zipco +"&distance=102&API_KEY=2193A50C-794E-49D8-AFD4-8353E4146A51")
+
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = ("Error..")
+
+        return render(request,'home.html',{'api': api})
+    else:
+        # api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=25&API_KEY=2193A50C-794E-49D8-AFD4-8353E4146A51")
+        # try:
+        #     api = json.loads(api_request.content)
+        # except Exception as e:
+        #     api = ("Error..")
+
+        return render(request,'home.html',{})
+    
+        
 
 
 def about(request):
